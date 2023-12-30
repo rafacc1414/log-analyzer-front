3e406679afac5a34080c8909f956e1dd620ed360
rafacc <rafacc1414@gmail.com>
2023-12-30 13:33:32 +0000

    Add web-socket service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FileDropComponent } from './file-drop/file-drop.component';
+import { WebSocketService } from './web-socket/web-socket.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,38 @@ import { FileDropComponent } from './file-drop/file-drop.component';
 })
 
 export class AppComponent {
+
+  constructor(private webSocketService: WebSocketService) { 
+  }
+
+  public messages: string[] = [];
+  public message: string = '';
+  
+  ngOnInit(): void {
+    this.webSocketService.connect().subscribe(
+      (message) => {
+        this.messages.push(message);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  sendMessage(): void {
+    this.webSocketService.sendMessage(this.message);
+    this.message = '';
+  }
+
+
   logs: File[] = [];
 
   onFilesDropped(files: FileList): void {
     this.logs = Array.from(files);
+    this.logs.forEach((file: File) => {
+      this.webSocketService.sendLogsFile(file);
+    });
   }
+
+
 }
---
+++ src/app/web-socket/web-socket.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WebSocketService {
+
+  private socket: WebSocket;
+
+  constructor() {
+    this.socket = new WebSocket('wss://localhost:3000');
+  }
+
+  public connect(): Observable<any> {
+    return new Observable(observer => {
+      this.socket.onmessage = (event) => observer.next(event.data);
+      this.socket.onerror = (event) => observer.error(event);
+      this.socket.onclose = () => observer.complete();
+    });
+  }
+
+  public sendMessage(message: string): void {
+    this.socket.send(message);
+  }
+
+  // Método para enviar archivos de logs
+  sendLogsFile(file: File) {
+      this.socket.send(file);
+  }
+
+}
